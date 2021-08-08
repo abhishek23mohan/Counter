4131947c7a50560a44b15a83e39058c87902f98a
mohanabhishek <abhishekm@prdxn.com>
2021-08-08 14:27:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//document.getElementById("count").innerHTML
+
+// let myAge = 25
+// // console.log(myAge)
+
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// let count = 0
+// console.log(count)
+
+// let bonusPoint = 50
+// console.log(bonusPoint)
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+// bonusPoint = bonusPoint - 75
+// console.log(bonusPoint)
+// bonusPoint = bonusPoint + 45
+// console.log(bonusPoint)
+
+function increment(){
+    console.log("The button was clicked")
+}
+
+let lap1 = 10
+let lap2 = 12
+let lap3 = 9
+
+function lapTime(){
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+lapTime()
